26a5db9beed8ce8889679c5286b7b1d1e4ab61c0
nikaidoumari <b1517914@planet.kanazawa-it.ac.jp>
2017-07-12 11:04:15 +0900

    Add source code.

---
+++ nwprinter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+namespace nwprinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start("explorer.exe", @"\\iogate2.kanazawa-it.ac.jp");
+        }
+    }
+}
